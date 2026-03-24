bc7f8a3dd66b44c977bfa9c4ef689ef1b4514d62
derricksoni <muliikaderricksoni@gmail.com>
2026-03-24 20:52:16 +0300

    Update ex2_fuel.c

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,25 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* MULIIKA DERRICK*/
+/* 25/U/BIO/05297/PD */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    //variable identification 
+    
+    float distance, fuel, efficiency;
+
+    printf("Enter distance travelled (km): ");
+    scanf("%f", &distance);
+
+    printf("Enter fuel used (litres): ");
+    scanf("%f", &fuel);
+
+    efficiency = distance / fuel;
+
+    printf("\nFuel efficiency: %.2f km/l\n", efficiency);
+
+    
 
     return 0;
 }
