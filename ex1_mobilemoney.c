22a1f86eae1e43616a48d907ba3d70619593f457
derricksoni <muliikaderricksoni@gmail.com>
2026-03-24 19:53:00 -0700

    Update ex1_mobilemoney.c

--- ex1_mobilemoney.c
+++ ex1_mobilemoney.c
@@ -2,15 +2,25 @@
 /*Student number:25/U /BIO/05297/PD*/
 #include <stdio.h>
 int main(){
-float  amount , fee_percent ,fee , total;
-printf("enter amount to send:");
-scanf("%f",&amount);
-printf("enter transaction fee(%%):");
-scanf("%f",&fee_percent );
-fee=(fee_percent /100)*amount;
-total= amount+fee;
-printf("\nTransaction fee:%.2f UGX\n",fee);
-printf("total deducted: %.2f\n");
+int amount_to_send;
+    float transaction_fee;
+    int total_aamount_deducted;
+
+
+   printf("ENTER AMOUNT TO SEND :  ");
+    scanf("%d", &amount_to_send);
+
+    printf("ENTER TRANSACTION FEE :  ");
+    scanf("%f", &transaction_fee);s
+
+    float fee= transaction_fee*amount_to_send/100;
+
+    printf("YOUR FEE AMOUNT IS : %.1f and  ",fee);
+
+    float amount_deducted = amount_to_send+fee;
+    printf("AMOUNT DEDUCTED IS : %.1f", amount_deducted);
+
+
 return 0;
  }
 
